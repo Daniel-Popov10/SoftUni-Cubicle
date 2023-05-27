1c24ecb354f09e5b747c7773f3445b5e1eaa1ad4
Daniel Popov <114098743+Daniel-Popov10@users.noreply.github.com>
2023-05-27 22:49:05 +0300

    added cube controller

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,12 @@
 // TODO: Require Controllers...
 const router = require("express").Router();
 const homeController = require("../controllers/homeController");
+const cubeController = require("../controllers/cubeController");
 
 // TODO...
 
 router.use(homeController);
+router.use("/cubes", cubeController);
 router.use("*", (req, res) => {
   res.redirect("/404");
 });
