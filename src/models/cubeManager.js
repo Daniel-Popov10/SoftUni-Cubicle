f27d6a7bd065d4e758277702e5f3ba4062bbd3c2
Daniel Popov <114098743+Daniel-Popov10@users.noreply.github.com>
2023-05-29 20:25:03 +0300

    added from & to filters

--- src/models/cubeManager.js
+++ src/models/cubeManager.js
@@ -16,16 +16,51 @@ const cubes = [
       "https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067-675x380.jpg",
     difficultyLevel: 3,
   },
+
+  {
+    id: "1n73sh8holhz66elc",
+    name: "Round cube",
+    description: "A cool mirror cube",
+    imageUrl:
+      "https://cdn.shopify.com/s/files/1/0270/0342/0758/files/cubes6_480x480.jpg?v=1609225666",
+    difficultyLevel: 6,
+  },
+  {
+    id: "1n73sh8holhz66elc",
+    name: "Floating cube",
+    description: "A cool mirror cube",
+    imageUrl:
+      "https://images.unsplash.com/photo-1562774439-23ae4c68ad6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cnViaWslMjBjdWJlfGVufDB8fDB8fHww&w=1000&q=80",
+    difficultyLevel: 5,
+  },
+
+  {
+    id: "1n73sh8holhz66elc",
+    name: "Hexagon cube",
+    description: "A cool mirror cube",
+    imageUrl: "https://www.reviewgeek.com/p/uploads/2022/02/3972bf2b.png",
+    difficultyLevel: 1,
+  },
 ];
 
 const cubeManager = {
   getAllCubes: (getAllCubes = (search, from, to) => {
     let result = cubes.slice();
+
     if (search) {
       result = result.filter((cube) =>
         cube.name.toLowerCase().includes(search.toLowerCase())
       );
     }
+
+    if (from) {
+      result = result.filter((cube) => cube.difficultyLevel >= Number(from));
+    }
+
+    if (to) {
+      result = result.filter((cube) => cube.difficultyLevel <= Number(to));
+    }
+
     return result;
   }),
 
