3e5ebde4c902fec8b685ca8ea0eefea5b7030437
Daniel Popov <114098743+Daniel-Popov10@users.noreply.github.com>
2023-06-08 18:52:54 +0300

    add create accessory in DB

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
     res.render('accessories/create');
+});
+
+router.post("/addAccessory", async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+    await accessoryManager.createAccessory({ name, description, imageUrl });
+    res.redirect('/');
 });
 
 
--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -2,10 +2,12 @@ const Accessory = require('../models/accessory');
 
 const accessoryManager = {
     createAccessory: async (accessoryData) => {
-        const accessory = new Accessory;
+        const accessory = new Accessory(accessoryData);
 
         await accessory.save();
 
         return accessory;
     }
 };
+
+module.exports = accessoryManager;
