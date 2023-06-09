6c64dd042b64378c9ee96dc16a7fbe531385ebda
Daniel Popov <114098743+Daniel-Popov10@users.noreply.github.com>
2023-06-09 13:57:30 +0300

    add attach accessory page and button

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -2,13 +2,18 @@ const router = require('express').Router();
 const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
-    res.render('accessories/create');
+    res.render('accessory/create');
 });
 
 router.post("/addAccessory", async (req, res) => {
     const { name, description, imageUrl } = req.body;
     await accessoryManager.createAccessory({ name, description, imageUrl });
     res.redirect('/');
+});
+
+router.get('/attach', (req, res) => {
+    console.log('accessory controller reached')
+    res.render('accessory/attach');
 });
 
 
