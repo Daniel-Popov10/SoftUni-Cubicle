bcbfca28bb1410baee0d8709ed6ef505ff1c09ad
Daniel Popov <114098743+Daniel-Popov10@users.noreply.github.com>
2023-06-21 02:22:16 +0300

    add accessory validations

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,14 +1,24 @@
 const router = require('express').Router();
 const accessoryManager = require('../managers/accessoryManager');
+const { extractErrorMessages } = require('../utils/errorHelpers');
 
 router.get('/create', (req, res) => {
     res.render('accessory/create');
 });
 
 router.post("/addAccessory", async (req, res) => {
+
     const { name, description, imageUrl } = req.body;
-    await accessoryManager.createAccessory({ name, description, imageUrl });
-    res.redirect('/');
+
+    try {
+        await accessoryManager.createAccessory({ name, description, imageUrl });
+        res.redirect('/');
+    } catch (err) {
+        const errorMessages = extractErrorMessages(err);
+
+        return res.status(400).render('cube/create', { errorMessages });
+    }
+
 });
 
 
--- src/models/accessory.js
+++ src/models/accessory.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
 const accessorySchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    imageUrl: String,
+    name: {
+        type: String,
+        minLength: [5, 'Accesory name must be at least 5 characters long!'],
+        match: [/^[A-Za-z0-9 ]+$/, 'Invalid characters, name must consist of only letters numbers and whitespaces!'],
+    },
+    description: {
+        type: String,
+        minLength: [20, 'Description must be at least 20 characters long!'],
+        match: [/^[A-Za-z0-9 ]+$/, 'Invalid characters, description must consist of only letters numbers and whitespaces!'],
+    },
+    imageUrl: {
+        type: String,
+        match: [/^[http:\/\/ | https:\/\/]+\w+\S+$/, 'Image must be a valid link starting with https or https!'],
+    },
 });
 
 const accessory = mongoose.model('Accessory', accessorySchema);
